f4f80a108378c733abf0df34e1a6896aef2a54a2
Vadim Safonov <imamatory@gmail.com>
2019-02-10 19:15:35 +0300

    --wip-- [skip ci]

--- src/tags.js
+++ src/tags.js
@@ -1,24 +1,24 @@
 const createNode = createContext => (name, attrs = {}, content = '') => {
-  const baseCtx = {
-    renderAttrs: attrsObj => Object.keys(attrsObj).map(key => ` ${key}=${attrs[key]}`).join(''),
-  };
-  return createContext({
-    ...baseCtx, name, attrs, content,
-  });
+  const baseCtx = { name, attrs, content };
+  baseCtx.renderAttrs = ctx => Object.keys(ctx.attrs).map(key => ` ${key}=${attrs[key]}`).join('');
+  baseCtx.isShort = ctx => ctx.toString().length > 10;
+  return createContext(baseCtx);
 };
 
-export const createPairedTag = createNode(ctx => ({
-  toString: () => {
+export const createPairedTag = createNode((ctx) => {
+  ctx.toString = () => {
     const {
-      name, attrs, content, renderAttrs,
+      name, content, renderAttrs,
     } = ctx;
-    return `<${name}${renderAttrs(attrs)}>${content}</${name}>`;
-  },
-}));
+    return `<${name}${renderAttrs(ctx)}>${content}</${name}>`;
+  };
+  return ctx;
+});
 
-export const createUnpairedTag = createNode(ctx => ({
-  toString: () => {
-    const { name, attrs, renderAttrs } = ctx;
-    return `<${name}${renderAttrs(attrs)} />`;
-  },
-}));
+export const createUnpairedTag = createNode((ctx) => {
+  ctx.toString = () => {
+    const { name, renderAttrs } = ctx;
+    return `<${name}${renderAttrs(ctx)} />`;
+  };
+  return ctx;
+});
